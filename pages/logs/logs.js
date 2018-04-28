acce0e1104559e4cd24c759673557527e9c8d05b
张豪 <hao.zhang517@chinaredstar.com>
2018-04-28 16:37:41 +0800

    订单页面支付

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -39,6 +39,63 @@ Page({
         }
     })
   },
+  toPay(e){
+    let orderId = e.currentTarget.dataset.orderid;
+       console.log('fffff',orderId);
+         //支付
+          let pay = 'https://www.sangyiwen.top/order/tenpay;JSESSIONID='+wx.getStorageSync("sessionId");
+          console.log(pay)
+           wx.request({
+              url: pay, //仅为示例，并非真实的接口地址
+              method:'POST',
+              data: {
+                orderId: orderId,
+              },
+              header: {
+                'content-type': 'application/json' // 默认值
+                },
+              success: function(res) {
+                if(res.data.code == 200){
+                   console.log('pay',res.data.value);
+                let datasss = {
+                  'timeStamp': res.data.value.timeStamp,  
+                                    'nonceStr': res.data.value.nonceStr,  
+                                    'package': res.data.value.package,  
+                                    'signType': 'MD5',  
+                                    'paySign': res.data.value.sign, 
+                };
+                                  console.log('========',datasss)
+                wx.requestPayment({  
+                                    'timeStamp': res.data.value.timeStamp,  
+                                    'nonceStr': res.data.value.nonceStr,  
+                                    'package': res.data.value.package,  
+                                    'signType': 'MD5',  
+                                    'paySign': res.data.value.sign,  
+                                    'success': function (succ) {  
+                                         wx.showModal({
+                                          title:'提示',
+                                          content:'付款成功，请取走您的美酒',
+                                          showCancel:false,
+                                          confirmText:'好的',
+                                          confirmColor:'#4485c5',  
+                                          success:function(res){
+                                            wx.reLaunch({
+                                              url: 'logs'
+                                            })
+                                          }                        
+                                        })
+                                        // t.showZanToast('付款成功，请取走您的美酒'); 
+                                    },
+                                    'fail':function(data){
+                                       t.showZanToast(data.errMsg); 
+                                    }
+                                }) 
+                }else{
+                  wx.showToast({title:res.data.msg,icon:'none'})
+                }
+                }
+            })
+  },
   onShow: function () {
     wx.showLoading({title:'加载中...'})
     let t = this;
